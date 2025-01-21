cb2a6626b269f695b4d6eb1e3c90005df59c46e6
Fabian Fuenmayor <fabianfuen19@icloud.com>
2025-01-21 11:00:18 +0000

    Chore(navbar): in process, google font added

--- src/front/js/store/flux.jsx
+++ src/front/js/store/flux.jsx
@@ -1,32 +1,32 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			current_user: null,
-            user_token: null,
-            citas: [],
-            especialistas: [],
-            paciente_citas: [],
-            disponibilidad: [],
+			// current_user: null,
+            // user_token: null,
+            // citas: [],
+            // especialistas: [],
+            // paciente_citas: [],
+            // disponibilidad: [],
 		},
 		actions: {
-			user_register: async (formData) => {
-				const url = "https://ideal-disco-97q4qxxw959v2pvwv-3001.app.github.dev/";
+			// user_register: async (formData) => {
+			// 	const url = "https://ideal-disco-97q4qxxw959v2pvwv-3001.app.github.dev/";
 				
-                try {
-                    const respo = await fetch(`${url}/api/register`, {
-                        method: "POST",
-                        headers: { 
-							"Content-Type": "application/json" 
-						},
-                        body: JSON.stringify(formData),
-                    });
-                    if (!respo.ok) throw new Error("Error en el registro");
-                    const data = await response.json();
-                    console.log("Usuario registrado:", data);
-                } catch (error) {
-                    console.error("Error en registro:", error);
-                }
-            },
+            //     try {
+            //         const respo = await fetch(`${url}/api/register`, {
+            //             method: "POST",
+            //             headers: { 
+			// 				"Content-Type": "application/json" 
+			// 			},
+            //             body: JSON.stringify(formData),
+            //         });
+            //         if (!respo.ok) throw new Error("Error en el registro");
+            //         const data = await response.json();
+            //         console.log("Usuario registrado:", data);
+            //     } catch (error) {
+            //         console.error("Error en registro:", error);
+            //     }
+            // },
 		}
 	};
 };
