1ed07016612234d81c7952e5a5582866eddacb7b
Fabian Fuenmayor <fabianfuen19@icloud.com>
2025-01-21 20:47:41 +0000

    chore(changes): navbar margin changes

--- src/front/js/component/navbar.jsx
+++ src/front/js/component/navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../../img/logo-medi-2.png";
+import { Link } from "react-router-dom";
 // import { Link } from "react-router-dom";
 
 export const Navbar = () => {
@@ -8,23 +9,26 @@ export const Navbar = () => {
 			<nav className="navbar navbar-expand-lg navbar-light pt-0 pb-0 navbar-principal-classes h-auto">
 				<div className="d-flex w-100">
 					<div className="d-flex w-100 align-items-center">
-						<img src={logo} alt="logo" className="nav-logo img-fluid" />
+						<Link to="/">
+							<img src={logo} alt="logo" className="nav-logo img-fluid" />
+						</Link>
+						
 						<ul className="navbar-nav ms-auto nav_links">
-							<li className="nav-item">
-								<a className="nav-link nav-text " aria-current="page" href="#">About us</a>
+							<li className="nav-item me-3">
+								<Link to="/" className="nav-link nav-text " aria-current="page">About us</Link>
 							</li>
 							<li className="nav-item">
-								<a className="nav-link nav-text" href="#">Support</a>
+								<Link to="/" className="nav-link nav-text">Support</Link>
 							</li>
 						</ul>
 					</div>
 					<div className="nav-btn-container">
-						<div className="btn custom-btn special_margin" type="submit">
+						<Link to="/" className="btn custom-btn special_margin" type="submit">
 							<p>Login</p>
-						</div>
-						<div className="btn custom-btn special_margin" type="submit">
+						</Link>
+						<Link to="/" className="btn custom-btn special_margin" type="submit">
 							<p>Register</p>
-						</div>
+						</Link>
 					</div>
 				</div>
 			</nav>
