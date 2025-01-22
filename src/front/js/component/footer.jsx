5da798b116a638c8af0287d6fdadea13b8e25e3a
Orangel Hernandez <orangel.hernandez1999@gmail.com>
2025-01-22 18:59:22 +0000

    feat: changes on fontsize

--- src/front/js/component/footer.jsx
+++ src/front/js/component/footer.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 export const Footer = () => (
     <footer>
-        <div className="bg-primary text-white py-5  d-flex justify-content-between align-items-center text-opacity-75"
-        style={{ fontFamily: 'Gantari, sans-serif', fontSize: '16px', background: 'linear-gradient(180deg, #6F4CB0 20%, #4632A5 100%)'}}>
+        <div className="bg-primary text-white py-2  d-flex justify-content-between align-items-center text-opacity-75"
+        style={{ fontFamily: 'Gantari, sans-serif', fontSize: '12px', background: 'linear-gradient(180deg, #6F4CB0 20%, #4632A5 100%)'}}>
             <div className="container ps-4">
             <img 
             src="https://github.com/FabFu19/MediAgenda-Project-fs-pt80/raw/799116be9c56823c46f063306d29671d34a2fd5b/src/front/img/image%205.png" 
@@ -14,12 +14,12 @@ export const Footer = () => (
             </div>
             <div className="container">
                 <Link to="/about" className="text-white text-decoration-none text-opacity-75">
-                    <span className="mb-0"style={{ fontFamily: 'Gantari, sans-serif', fontSize: '16px' }}>About us</span>
+                    <span className="mb-0"style={{ fontFamily: 'Gantari, sans-serif', fontSize: '12px' }}>About us</span>
                 </Link>
             </div>
             <div className="container">
             <Link to="/support" className="text-white text-decoration-none text-opacity-75">
-            <span className="mb-0"style={{ fontFamily: 'Gantari, sans-serif', fontSize: '16px' }}>Support</span>
+            <span className="mb-0"style={{ fontFamily: 'Gantari, sans-serif', fontSize: '12px' }}>Support</span>
                 </Link>
             </div>
             <div className="container">
