82088b418f0be10c0e454be703f4d07cb95ddd6b
Fabian Fuenmayor <fabianfuen19@icloud.com>
2025-01-25 20:30:46 +0000

    chore(navbar): navbar for each section created until now

--- src/front/js/component/navbar.jsx
+++ src/front/js/component/navbar.jsx
@@ -8,8 +8,128 @@ export const Navbar = () => {
 
 
     const isProfilePage = location.pathname === "/profile";
+    const isLoginPage = location.pathname === "/login";
+    const isRegisterPage = location.pathname === "/register";
+    const isManageAvPage = location.pathname === "/book_appointment";
     const ProfileNavbar = () => (
         <nav className="navbar navbar-expand-lg navbar-light pt-0 pb-0 navbar-profile ">
+            <div className={`d-flex w-100 ${window.innerWidth > 768 ? "container" : ""}`}>
+                <Link to="/" className="nav-content-img">
+                    <img src={logo} alt="logo" className="nav-logo" />
+                </Link>
+                <div className={`align-items-center nav-container-items ${isOpen && "open"}`}>
+                    <ul className="navbar-nav nav_links">
+                        <div className="nav-btn-container-responsive">
+                            <Link to="/" className="btn custom-btn special_margin" type="submit">
+                                <p>Logout</p>
+                            </Link>
+                        </div>
+                    </ul>
+                </div>
+                <li className="nav-btn-container">
+                    <Link to="/" className="btn custom-btn special_margin" type="submit">
+                        <p>Logout</p>
+                    </Link>
+                </li>
+                <div className={`nav-toggle ${isOpen && "open"}`} onClick={() => setIsOpen(!isOpen)}>
+                    <span></span>
+                    <span></span>
+                    <span className="bar-special-style"></span>
+                </div>
+            </div>
+        </nav>
+    );
+
+    const LoginNav = () => (
+        <nav className="navbar navbar-expand-lg navbar-light pt-0 pb-0 navbar-login ">
+            <div className={`d-flex w-100 ${window.innerWidth > 768 ? "container" : ""}`}>
+                <Link to="/" className="nav-content-img">
+                    <img src={logo} alt="logo" className="nav-logo" />
+                </Link>
+                <div className={`align-items-center nav-container-items ${isOpen && "open"}`}>
+                    <ul className="navbar-nav nav_links">
+                        <li className="nav-item me-3 nav-text-content">
+                            <Link to="/" className="nav-link nav-text" aria-current="page">
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item me-3 nav-text-content">
+                            <Link to="/" className="nav-link nav-text" aria-current="page">
+                                About us
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link nav-text">
+                                Support
+                            </Link>
+                        </li>
+                        <div className="nav-btn-container-responsive">
+                            <Link to="/" className="btn custom-btn special_margin" type="submit">
+                                <p>Register</p>
+                            </Link>
+                        </div>
+                    </ul>
+                </div>
+                <li className="nav-btn-container">
+                    <Link to="/" className="btn custom-btn special_margin" type="submit">
+                        <p>Register</p>
+                    </Link>
+                </li>
+                <div className={`nav-toggle ${isOpen && "open"}`} onClick={() => setIsOpen(!isOpen)}>
+                    <span></span>
+                    <span></span>
+                    <span className="bar-special-style"></span>
+                </div>
+            </div>
+        </nav>
+    );
+
+    const RegisterNav = () => (
+        <nav className="navbar navbar-expand-lg navbar-light pt-0 pb-0 navbar-login ">
+            <div className={`d-flex w-100 ${window.innerWidth > 768 ? "container" : ""}`}>
+                <Link to="/" className="nav-content-img">
+                    <img src={logo} alt="logo" className="nav-logo" />
+                </Link>
+                <div className={`align-items-center nav-container-items ${isOpen && "open"}`}>
+                    <ul className="navbar-nav nav_links">
+                        <li className="nav-item me-3 nav-text-content">
+                            <Link to="/" className="nav-link nav-text" aria-current="page">
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item me-3 nav-text-content">
+                            <Link to="/" className="nav-link nav-text" aria-current="page">
+                                About us
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link nav-text">
+                                Support
+                            </Link>
+                        </li>
+                        <div className="nav-btn-container-responsive">
+                            <Link to="/" className="btn custom-btn special_margin" type="submit">
+                                <p>Login</p>
+                            </Link>
+                        </div>
+                    </ul>
+                </div>
+                <li className="nav-btn-container">
+                    <Link to="/" className="btn custom-btn special_margin" type="submit">
+                        <p>Login</p>
+                    </Link>
+                </li>
+                <div className={`nav-toggle ${isOpen && "open"}`} onClick={() => setIsOpen(!isOpen)}>
+                    <span></span>
+                    <span></span>
+                    <span className="bar-special-style"></span>
+                </div>
+            </div>
+        </nav>
+    );
+
+    const BookAppoNav = () => (
+        <nav className="navbar navbar-expand-lg navbar-light pt-0 pb-0 navbar-profile nav-book ">
             <div className={`d-flex w-100 ${window.innerWidth > 768 ? "container" : ""}`}>
                 <Link to="/" className="nav-content-img">
                     <img src={logo} alt="logo" className="nav-logo" />
@@ -83,5 +203,12 @@ export const Navbar = () => {
         </nav>
     );
 
-    return <>{isProfilePage ? <ProfileNavbar /> : <GeneralNavbar />}</>;
+    return <><>
+    {isProfilePage && <ProfileNavbar />}
+    {isLoginPage && <LoginNav />}
+    {isRegisterPage && <RegisterNav />}
+    {isManageAvPage && <BookAppoNav />}
+
+    {!isProfilePage && !isLoginPage && !isRegisterPage && !isManageAvPage && <GeneralNavbar />}
+</></>;
 };
