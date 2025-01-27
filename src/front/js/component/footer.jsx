92b8d4fcd7c2cceea1faba4823f2595285a793db
Orangel Hernandez <orangel.hernandez1999@gmail.com>
2025-01-27 20:24:54 +0000

    changes on footer to be more responsive

--- src/front/js/component/footer.jsx
+++ src/front/js/component/footer.jsx
@@ -5,24 +5,26 @@ import logo_footer from "../../img/logo_footer.png";
 export const Footer = () => (
   <footer className="footer">
     <div className="footer-body">
-      <div className="container d-flex">
-        <img
-          className="footer-logo"
-          src={logo_footer}
-          alt="Logo"
-        />
+      <div className="container"> 
+        <div className="row justify-content-between align-items-center text-center row-footer"> 
+          <div className="col-lg-3 col-md-6 col-sm-12">
+            <img className="footer-logo" src={logo_footer} alt="Logo" />
+          </div>
+          <div className="col-lg-2 col-md-6 col-sm-12  mb-3 mb-lg-0">
+            <Link to="/about" className="footer-link">
+              About us
+            </Link>
+          </div>
+          <div className="col-lg-2 col-md-6 col-sm-12  mb-3 mb-lg-0">
+            <Link to="/support" className="footer-link">
+              Support
+            </Link>
+          </div>
+          <div className="col-lg-3 col-md-6 col-sm-12">
+            © 2025 MediAgenda. All rights reserved.
+          </div>
+        </div>
       </div>
-      <div className="container">
-        <Link to="/about" className="footer-link">
-          <span>About us</span>
-        </Link>
-      </div>
-      <div className="container">
-        <Link to="/support" className="footer-link">
-          <span>Support</span>
-        </Link>
-      </div>
-      <div className="container">© 2025 MediAgenda. All rights reserved.</div>
     </div>
   </footer>
 );
