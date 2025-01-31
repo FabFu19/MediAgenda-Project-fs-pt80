b0287c6c848cd3e66ccb3babc9796dfaa8de28cd
Fabian Fuenmayor <fabianfuen19@icloud.com>
2025-01-31 10:12:00 +0000

    endopoints added with google api calendar added

--- src/api/routes.py
+++ src/api/routes.py
@@ -7,12 +7,9 @@ from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 from datetime import datetime, timedelta, timezone
-
-from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google_auth_oauthlib.flow import Flow
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 import os
 
 api = Blueprint('api', __name__)
@@ -25,7 +22,7 @@ SCOPES = ["https://www.googleapis.com/auth/calendar"]
 flow = Flow.from_client_secrets_file(
     CLIENT_SECRET_FILE,
     scopes=SCOPES,
-    redirect_uri="https://glowing-succotash-5g4p4995q9vw2v6q6-3001.app.github.dev/redirect"
+    redirect_uri="https://glowing-succotash-5g4p4995q9vw2v6q6-3000.app.github.dev"
 )
 
 
@@ -224,293 +221,107 @@ def cancel_calendly_appointment(appointment_id):
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-# @api.route('/auth/google', methods=['GET'])
-# def google_auth():
-#     flow = Flow.from_client_secrets_file(
-#         CLIENT_SECRET_FILE, 
-#         scopes=SCOPES, 
-#         redirect_uri= "https://glowing-succotash-5g4p4995q9vw2v6q6-3001.app.github.dev"
-#     )
-    
-#     auth_url, state = flow.authorization_url(prompt="consent")
-  
-#     session["oauth_state"] = state
+@api.route('/auth/google', methods=['GET'])
+def google_auth():
+    auth_url, state = flow.authorization_url(prompt="consent")
+    session["oauth_state"] = state
+    return jsonify({"auth_url": auth_url})
 
-#     return jsonify({"auth_url": auth_url})
+@api.route('/auth/google/callback', methods=['GET'])
+def google_callback():
+    try:
+        flow.fetch_token(authorization_response=request.url)
+        credentials = flow.credentials
+        return jsonify({
+            "access_token": credentials.token,
+            "refresh_token": credentials.refresh_token,
+            "token_uri": credentials.token_uri,
+            "client_id": credentials.client_id,
+            "client_secret": credentials.client_secret,
+        }), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-
-# @api.route('/auth/google/callback', methods=['GET'])
-# def google_callback():
-#     try:
-#         flow = Flow.from_client_secrets_file(
-#         CLIENT_SECRET_FILE, 
-#         scopes=SCOPES, 
-#         redirect_uri="https://glowing-succotash-5g4p4995q9vw2v6q6-3001.app.github.dev")
-
-#         # if "oauth_state" not in session or session["oauth_state"] != request.args.get("state"):
-#         #     return jsonify({"error": "CSRF Warning! State mismatch."}), 400
-
-#         flow.fetch_token(authorization_response=request.url)
-#         credentials = flow.credentials
-
-#         return jsonify({
-#             "access_token": credentials.token,
-#             "refresh_token": credentials.refresh_token,
-#             "token_uri": credentials.token_uri,
-#             "client_id": credentials.client_id,
-#             "client_secret": credentials.client_secret,
-#         }), 200
-
-#     except Exception as e:
-#         return jsonify({"error": str(e)}), 500
-
-# @api.route('/disponibilidad', methods=['POST'])
-# @jwt_required()
-# def crear_disponibilidad():
-#     try:
-#         current_user = get_jwt_identity()
-#         especialista = Especialistas.query.filter_by(user_id = current_user).first() 
-#         if not especialista: 
-#             return jsonify({'error': 'No autorizado'})
-#         data = request.json
-#         fecha = data.get('fecha')
-#         hora_inicio = data.get('hora_inicio')
-#         hora_final = data.get('hora_final')
-
-#         credentials = Credentials(data.get("access_token"))
-#         service = build("calendar", "v3", credentials=credentials)
+@api.route('/disponibilidad', methods=['POST'])
+@jwt_required()
+def crear_disponibilidad():
+    try:
+        current_user = get_jwt_identity()
+        especialista = Especialistas.query.filter_by(user_id=current_user).first()
+        if not especialista:
+            return jsonify({'error': 'No autorizado'}), 403
         
-#         start_time = f"{fecha}T{hora_inicio}:00"
-#         end_time = f"{fecha}T{hora_final}:00"
+        data = request.json
+        fecha = data['fecha']
+        hora_inicio = data['hora_inicio']
+        hora_final = data['hora_final']
+        credentials = Credentials(data.get("access_token"))
+        service = build("calendar", "v3", credentials=credentials)
 
-#         event_body = {
-#             "summary": "Disponibilidad del Médico",
-#             "description": f"El Dr. {especialista.user.nombre} {especialista.user.apellido} está disponible en este horario.",
-#             "start": {"dateTime": start_time, "timeZone": "Etc/GMT"},
-#             "end": {"dateTime": end_time, "timeZone": "Etc/GMT"},
-#             "transparency": "transparent",  
-#             "visibility": "public",
-#         }
-#         calendar = service.calendars().insert(calendarId="primary", body=event_body).execute()
+        start_time = f"{fecha}T{hora_inicio}:00"
+        end_time = f"{fecha}T{hora_final}:00"
 
-#         nueva_disponibilidad = DisponibilidadMedico(
-#             medico_id= especialista.id,
-#             fecha= data['fecha'],
-#             hora_inicio= hora_inicio,
-#             hora_final= hora_final,
-#             is_available= True,
-#             google_event_id=calendar["id"]
-#         )
-#         db.session.add(nueva_disponibilidad)
-#         db.session.commit()
+        event_body = {
+            "summary": "Disponibilidad del Médico",
+            "description": f"El Dr. {especialista.user.nombre} {especialista.user.apellido} está disponible en este horario.",
+            "start": {"dateTime": start_time, "timeZone": "America/New_York"},
+            "end": {"dateTime": end_time, "timeZone": "America/New_York"},
+        }
 
-#         return jsonify({"msg": "Disponibilidad creada exitosamente"}), 201
-
-#     except Exception as e:
-#         db.session.rollback()
-#         return jsonify({"error": str(e)}), 500
-
-# @api.route('/disponibilidad/<int:id>', methods=['PUT'])
-# @jwt_required()
-# def actualizar_disponibilidad(id):
-#     try:
-#         current_user = get_jwt_identity()
-#         especialista = Especialistas.query.filter_by(user_id=current_user).first()
-#         if not especialista:
-#             return jsonify({'error': 'No autorizado'})
-
-#         disponibilidad = DisponibilidadMedico.query.get(id)
-#         if not disponibilidad or disponibilidad.medico_id != especialista.id:
-#             return jsonify({'error': 'Disponibilidad no encontrada'}), 404
-
-#         data = request.json
-#         fecha = data.get('fecha')
-#         hora_inicio = data.get('hora_inicio')
-#         hora_final = data.get('hora_final')
-
+        event = service.events().insert(calendarId="primary", body=event_body).execute()
         
-#         disponibilidad.fecha = fecha
-#         disponibilidad.hora_inicio = hora_inicio
-#         disponibilidad.hora_final = hora_final
-#         db.session.commit()
+        nueva_disponibilidad = DisponibilidadMedico(
+            medico_id=especialista.id,
+            fecha=fecha,
+            hora_inicio=hora_inicio,
+            hora_final=hora_final,
+            is_available=True,
+            google_event_id=event["id"]
+        )
+        db.session.add(nueva_disponibilidad)
+        db.session.commit()
 
-       
-#         if disponibilidad.google_event_id:
-#             credentials = Credentials(data.get("access_token"))
-#             service = build("calendar", "v3", credentials=credentials)
+        return jsonify({"msg": "Disponibilidad creada con éxito", "event_id": event["id"]}), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
 
-#             start_time = f"{fecha}T{hora_inicio}:00"
-#             end_time = f"{fecha}T{hora_final}:00"
+@api.route('/citas', methods=['POST'])
+@jwt_required()
+def agendar_cita():
+    try:
+        current_user = get_jwt_identity()
+        data = request.json
+        medico = Especialistas.query.get(data['medico_id'])
+        fecha = data['appointment_date']
+        hora = data['appointment_time']
+        credentials = Credentials(data.get("access_token"))
+        service = build("calendar", "v3", credentials=credentials)
 
-#             event_body = {
-#                 "summary": "Disponibilidad del Médico",
-#                 "description": f"El Dr. {especialista.user.nombre} {especialista.user.apellido} ha actualizado su disponibilidad.",
-#                 "start": {"dateTime": start_time, "timeZone": "Etc/GMT"},
-#                 "end": {"dateTime": end_time, "timeZone": "Etc/GMT"},
-#                 "transparency": "transparent",
-#                 "visibility": "public",
-#             }
+        start_time = f"{fecha}T{hora}:00"
+        end_time = f"{fecha}T{int(hora.split(':')[0]) + 1}:00"
 
-#             updated_event = service.events().update(
-#                 calendarId="primary", eventId=disponibilidad.google_event_id, body=event_body
-#             ).execute()
+        event_body = {
+            "summary": f"Cita con Dr. {medico.user.nombre} {medico.user.apellido}",
+            "start": {"dateTime": start_time, "timeZone": "America/New_York"},
+            "end": {"dateTime": end_time, "timeZone": "America/New_York"},
+            "attendees": [{"email": medico.user.email}]
+        }
 
-#             return jsonify({"msg": "Disponibilidad actualizada con éxito", "event": updated_event}), 200
-
-#         return jsonify({"msg": "Disponibilidad actualizada en la base de datos"}), 200
-
-#     except Exception as e:
-#         db.session.rollback()
-#         return jsonify({"error": str(e)}), 500
-
-# @api.route('/disponibilidad/<int:id>', methods=['DELETE'])
-# @jwt_required()
-# def eliminar_disponibilidad(id):
-#     try:
-#         current_user = get_jwt_identity()
-#         especialista = Especialistas.query.filter_by(user_id=current_user).first()
-#         if not especialista:
-#             return jsonify({'error': 'No autorizado'})
-
-#         disponibilidad = DisponibilidadMedico.query.get(id)
-#         if not disponibilidad or disponibilidad.medico_id != especialista.id:
-#             return jsonify({'error': 'Disponibilidad no encontrada'}), 404
-
-       
-#         if disponibilidad.google_event_id:
-#             credentials = Credentials(request.json.get("access_token"))
-#             service = build("calendar", "v3", credentials=credentials)
-
-#             service.events().delete(calendarId="primary", eventId=disponibilidad.google_event_id).execute()
-
+        event = service.events().insert(calendarId="primary", body=event_body).execute()
         
-#         db.session.delete(disponibilidad)
-#         db.session.commit()
+        nueva_cita = Citas(
+            paciente_id=current_user,
+            medico_id=medico.id,
+            estado='pendiente',
+            appointment_date=fecha,
+            appointment_time=hora,
+            google_event_id=event["id"]
+        )
+        db.session.add(nueva_cita)
+        db.session.commit()
 
-#         return jsonify({"msg": "Disponibilidad eliminada con éxito"}), 200
-
-#     except Exception as e:
-#         db.session.rollback()
-#         return jsonify({"error": str(e)}), 500
-
-
-# @api.route('/citas', methods=['POST'])
-# @jwt_required()
-# def agendar_cita():
-#     try:
-#         current_user = get_jwt_identity()
-        
-#         data = request.json
-#         medico = Especialistas.query.get(data.get('medico_id'))
-#         fecha = data.get('appointment_date')
-#         hora = data.get('appointment_time')
-
-#         credentials = Credentials(data.get("access_token"))
-#         service = build("calendar", "v3", credentials=credentials)
-#         start_time = f"{fecha}T{hora}:00"
-#         end_time = datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%S") + timedelta(hours=1)
-
-#         event_body = {
-#             "summary": f"Cita con {medico.user.nombre} {medico.user.apellido}",
-#             "start": {"dateTime": start_time, "timeZone": "Etc/GMT"},
-#             "end": {"dateTime": end_time.isoformat(), "timeZone": "Etc/GMT"},
-#             "attendees": [{"email": medico.user.email}, {"email": current_user.user.email}],
-#         }
-#         calendar = service.calendars().insert(calendarId="primary", body=event_body).execute()
-
-#         nueva_cita = Citas(
-#             paciente_id=current_user, 
-#             medico_id=data.get('medico_id'),
-#             estado='pendiente',
-#             appointment_date=fecha,
-#             appointment_time=hora,
-#             notes=data.get('notes', ''),
-#             google_event_id=calendar["id"]
-#         )
-#         db.session.add(nueva_cita)
-#         db.session.commit()
-
-#         return jsonify({"msg": "Cita agendada exitosamente", }), 201
-
-#     except Exception as e:
-#         db.session.rollback()
-#         print("Error al agendar cita:", str(e)) 
-#         return jsonify({"error": str(e)}), 500
-
-
-# @api.route('/citas', methods=['GET'])
-# @jwt_required()
-# def list_citas():
-#     try:
-#         current_user = get_jwt_identity()
-
-#         if current_user:
-#             citas = Citas.query.filter_by(paciente_id=current_user).all()
-#         else:
-#             citas = Citas.query.filter_by(medico_id=current_user['medico_id']).all()
-
-#         return jsonify([cita.serialize() for cita in citas]), 200
-
-#     except Exception as e:
-#         return jsonify({"error": str(e)}), 500
-
-# @api.route('/citas/actualizar/<int:id>', methods=['PUT'])
-# @jwt_required()
-# def actualizar_cita(id):
-#     try:
-#         current_user = get_jwt_identity()
-#         cita = Citas.query.get(id)
-
-#         if not cita or cita.paciente_id != current_user:
-#             return jsonify({"error": "No autorizado"}), 403
-
-#         data = request.json
-#         nueva_fecha = data.get('fecha', cita.appointment_date)
-#         nueva_hora = data.get('hora', cita.appointment_time)
-
-     
-#         credentials = Credentials(request.json.get("access_token"))
-#         service = build("calendar", "v3", credentials=credentials)
-#         start_time = f"{nueva_fecha}T{nueva_hora}:00"
-#         end_time = datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%S") + timedelta(hours=1)
-
-#         event_body = {
-#             "start": {"dateTime": start_time, "timeZone": "Etc/GMT"},
-#             "end": {"dateTime": end_time.isoformat(), "timeZone": "Etc/GMT"},
-#         }
-#         service.events().update(calendarId="primary", eventId=cita.google_event_id, body=event_body).execute()
-
-#         cita.appointment_date = nueva_fecha
-#         cita.appointment_time = nueva_hora
-#         db.session.commit()
-#         return jsonify({"msg": "Cita actualizada exitosamente"}), 200
-
-#     except Exception as e:
-#         return jsonify({"error": str(e)}), 500
-
-# @api.route('/citas/cancelar/<int:id>', methods=['DELETE'])
-# @jwt_required()
-# def cancelar_cita(id):
-#     try:
-#         current_user = get_jwt_identity()
-#         cita = Citas.query.get(id)
-
-#         if not cita or cita.paciente_id != current_user:
-#             return jsonify({"error": "No autorizado"}), 403
-
-       
-#         credentials = Credentials(request.json.get("access_token"))
-#         service = build("calendar", "v3", credentials=credentials)
-#         service.events().delete(calendarId="primary", eventId=cita.google_event_id).execute()
-
-#         db.session.delete(cita)
-#         db.session.commit()
-#         return jsonify({"msg": "Cita cancelada exitosamente"}), 200
-
-#     except Exception as e:
-#         db.session.rollback()
-#         return jsonify({"error": str(e)}), 500
-    
-
-
-
+        return jsonify({"msg": "Cita agendada con éxito", "event_id": event["id"]}), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
